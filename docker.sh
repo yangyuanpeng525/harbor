fe2045ed6f6eb1a114a59eaa7c289f47cc648576
yangyuanpeng <65752758+yangyuanpeng525@users.noreply.github.com>
2020-08-26 17:34:39 +0800

    Delete docker.sh

--- docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -it -d  --name harbor-offline-installer  registry.cn-hangzhou.aliyuncs.com/yangyuanpeng/harbor:harbor-offline-installer-v2.0.1.tgz
-sleep 5
-docker cp harbor-offline-installer:/harbor-offline-installer-v2.0.1.tgz  .
