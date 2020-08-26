0b026a7d4c42830e28d9e36105a7e1947e6f38e5
yangyuanpeng <65752758+yangyuanpeng525@users.noreply.github.com>
2020-08-26 17:42:01 +0800

    Update get_harbor.sh

--- get_harbor.sh
+++ get_harbor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -it -d --rm --name harbor-offline-installer  registry.cn-hangzhou.aliyuncs.com/yangyuanpeng/harbor:harbor-offline-installer-v2.0.1.tgz
-sleep 5
+docker run -it -d --rm --name harbor-offline-installer  registry.cn-hangzhou.aliyuncs.com/yangyuanpeng/harbor:harbor-offline-installer-v2.0.1.tgz  &>/dev/null 
+sleep 5  
 docker cp harbor-offline-installer:/harbor-offline-installer-v2.0.1.tgz  .
 sleep 5
-docker stop harbor-offline-installer 
+docker stop harbor-offline-installer  &>/dev/null
